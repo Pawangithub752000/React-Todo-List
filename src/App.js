242a3545a485d18aa820b3a2c1d7a17aad79e5ed
Pawan Kumar <pawanbtechcse752000@gmail.com>
2022-12-24 16:47:37 +0530

    Front-end part done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React from 'react';
+import trashIcon from './trash.svg'
+
+function ListItem(props) {
+  return (
+    <div className="list-item row jc-space-between">
+      <span>List Item</span>
+      <img src={trashIcon} className="delete-icon"/>
+    </div>
+
+  )
+}
 
 function App() {
   return (
     <div className="app-background">
-     <p className="heading-text">React To Do List 🔥</p>
-     <div className="task-container">
-      <div>
-        <input className="text-input"/>
-        <button>ADD</button>
+      <p className="heading-text">React To Do List 🔥</p>
+      <div className="task-container">
+        <div>
+          <input className="text-input" />
+          <button className="add-button">ADD</button>
+        </div>
+        <p className="no-item-text">No Task Added !</p>
+        <ListItem />
       </div>
-     </div>
+      <p className="footer-text">The Pawan Prajapati</p>
     </div>
   );
+
 }
 
 export default App;
